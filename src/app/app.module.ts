665f5a8f02f2c1dffd8420e97c808a46f7649f81
Yguanira <yguanirap@gmail.com>
2020-01-16 13:44:23 +0000

    Implementado y arreglado el routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+
 //Cosas de Firebase
 import { AngularFireModule } from 'angularfire2';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
@@ -11,6 +12,9 @@ import { environment } from '../environments/environment';
 import { ProductsComponent } from './products/products.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component'
+
+import appRoutes from './routerConfig';
 
 @NgModule({
   declarations: [
@@ -18,12 +22,14 @@ import { FooterComponent } from './components/footer/footer.component';
     ProductsComponent,
     HeaderComponent,
     FooterComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule, 
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent, ProductsComponent]
